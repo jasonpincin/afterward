d2019b9cc800539576d07e40ee4c330d60f47286
Jason Pincin <jason@pincin.com>
2015-08-24 14:24:32 -0400

    do not need a bind here

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var setImmediate = require('timers').setImmediate
 module.exports = function afterward (done, cb) {
     cb = cb || function () {}
     done.then(function (value) {
-        setImmediate(cb.bind(undefined, null, value))
+        setImmediate(cb, null, value)
     })
     done.catch(function (err) {
-        setImmediate(cb.bind(undefined, err || new Error('rejected with no reason')))
+        setImmediate(cb, err || new Error('rejected with no reason'))
     })
     return done
 }
