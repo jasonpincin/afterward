5983087e2c31fe5cdc6d3c7f472a149e673d9037
Jason Pincin <jason@pincin.com>
2015-11-12 20:43:52 -0500

    update tests to not use words reserved by old browsers

--- test/afterward.js
+++ test/afterward.js
@@ -9,7 +9,7 @@ test('afterward', function (t) {
 
     var p1 = Promise.resolve('hi')
     afterward(p1, function (err, data) {
-        t.false(err, 'err is falsey on resolution')
+        t.notOk(err, 'err is falsey on resolution')
         t.equal(data, 'hi', 'should invoke cb with resolved data')
     })
     t.doesNotThrow(function () {
@@ -18,14 +18,14 @@ test('afterward', function (t) {
 
     var p2 = Promise.reject()
     afterward(p2, function (err, data) {
-        t.true(err, 'err is truthy on reject(null)')
-        t.false(data, 'should invoke cb with no data on reject')
+        t.ok(err, 'err is truthy on reject(undefined)')
+        t.notOk(data, 'should invoke cb with no data on reject')
     })
 
     var p3 = Promise.reject(new Error('test'))
     afterward(p3, function (err, data) {
         t.equal(err.message, 'test', 'err.message is known on reject(err)')
-        t.false(data, 'should invoke cb with no data on reject')
+        t.notOk(data, 'should invoke cb with no data on reject')
     })
 
     var p4a = new Promise(function (resolve) {
@@ -34,7 +34,7 @@ test('afterward', function (t) {
         }, 100)
     })
     var p4b = afterward(p4a, function (err, msg) {
-        t.false(err)
+        t.notOk(err)
         t.equal(msg, 'hi', 'executes callback after async op')
     })
     t.equal(p4a, p4b, 'returns promise given')
